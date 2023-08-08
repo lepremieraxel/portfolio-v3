28afc7111ff73a8230d503921cb30b806d606157
ค૪૯Ն <axelmarcial151003@gmail.com>
2023-08-08 13:41:10 +0200

    test sessionStorage to localStorage

--- src/js/cookiePopup.js
+++ src/js/cookiePopup.js
@@ -5,19 +5,19 @@ cookieBtn.forEach(btn => {
   btn.onclick = () => {
     let isAccept = btn.getAttribute('data-cookie') == 'accept' ? true : false;
     if(isAccept){
-      sessionStorage.setItem('cookieState', 'accepted');
+      localStorage.setItem('cookieState', 'accepted');
       cookiePopup.classList.remove('active');
     } else {
-      sessionStorage.setItem('cookieState', 'denied');
+      localStorage.setItem('cookieState', 'denied');
       cookiePopup.classList.remove('active');
     }
   }
 });
 
 window.onload = () => {
-  if(sessionStorage.getItem('cookieState') == null){
+  if(localStorage.getItem('cookieState') == null){
     cookiePopup.classList.add('active');
-  } else if(sessionStorage.getItem('cookieState') == 'accepted' || sessionStorage.getItem('cookieState') == 'denied'){
+  } else if(localStorage.getItem('cookieState') == 'accepted' || localStorage.getItem('cookieState') == 'denied'){
     cookiePopup.classList.remove('active');
   }
   pfelHover();
